eb0573030c8c0da102dff700f9ef51cf456d58e9
Quan <lehuuminhquan2004@gmail.com>
2026-03-11 17:20:44 +0700

    api dang nhap + dang ky

---
+++ Backend/db.js
@@ -0,0 +1,16 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "dbshop"
+});
+db.connect((err) => {
+    if (err) {
+        console.error("Database conect failed:", err);
+    } else {
+        console.log("MySQL Connected...");
+    }
+});
+module.exports = db;
