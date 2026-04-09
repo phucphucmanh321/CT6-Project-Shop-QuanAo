18f9032d637091ac7c530d13e8611ed5d7f11d1c
Quan <lehuuminhquan2004@gmail.com>
2026-04-09 23:31:04 +0700

    xoa rac

--- Backend/userRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { getAllUsers, getUserById } = require("../controllers/userController");
-
-router.get("/", getAllUsers);
-router.get("/:id", getUserById);
-
-module.exports = router;
